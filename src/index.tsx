3902b19c8d0efe9a297fa5984af71b3849fcbd04
min <kabc3238@gmail.com>
2026-04-22 15:27:35 +0000

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx"; // 우리가 만든 App.jsx를 불러옵니다.
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
